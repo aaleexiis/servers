340e948cff9559cff250297fd83f7657270932ed
mladenovic_a <mladenovic@comprigo.com>
2019-01-16 11:40:15 +0100

    Implement basic login form

---
+++ src/components/loginForm/FullName.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>Full Name: {{firstName}} {{lastName}}</p>
+</template>
+<script>
+    export default {
+        props: {
+            firstName: {
+                type: String,
+                required: true
+            },
+            lastName: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
